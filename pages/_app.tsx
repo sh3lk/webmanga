94c6639b8985a4de7b2e86ba81f6793ab3bf8e9e
sh3lk <sh3lkoplyasov@gmail.com>
2022-07-16 22:40:35 +0300

    Tweak

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,22 @@
-import type { AppProps } from "next/app";
-import MainLayout from "components/layouts/main-layout";
-import ThemeConfig from "components/theme";
-import GlobalStyles from "@/components/theme/global-styles";
-import CssBaseline from "@mui/material/CssBaseline";
 import Head from "next/head";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 import { CacheProvider } from "@emotion/react";
 import { EmotionCache } from "@emotion/cache";
-import { SessionProvider } from "next-auth/react";
+import CssBaseline from "@mui/material/CssBaseline";
+
+import MainLayout from "components/layouts/main-layout";
+import GlobalStyles from "components/theme/global-styles";
+import ThemeConfig from "components/theme";
+
+import ApolloProvider from "lib/apollo/provider";
 import { createEmotionCache } from "lib/common/utils";
 import { REVALIDATE_INTERVAL } from "lib/constants";
-import ApolloProvider from "lib/apollo/provider";
+
+import { AnimatePresence } from "framer-motion";
+
 const clientSideEmotionCache = createEmotionCache();
-import { AnimatePresence } from "framer-motion"
+
 interface MyAppProps extends AppProps {
   emotionCache: EmotionCache;
 }
