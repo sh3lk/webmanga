ae3fef9ba23990ba1a2313df1eba2f1836f31d4e
sh3lk <sh3lkoplyasov@gmail.com>
2022-07-17 15:38:23 +0300

    Tweak

--- pages/me/index.tsx
+++ pages/me/index.tsx
@@ -2,7 +2,9 @@ import type { NextPage } from "next";
 import {useEffect} from "react";
 import {useSession} from "next-auth/react";
 import {useRouter} from "next/router";
+
 import Loader from "components/loader";
+
 const Me: NextPage = () => {
   const router = useRouter();
   const { data: session, status } = useSession();
